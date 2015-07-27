75bfa8148988ed98ea097787d7c5e15b5ab757ac
jgeofil <jeremy@thehyve.nl>
2015-07-27 17:27:20 +0200

    .

--- R/zzz.R
+++ R/zzz.R
@@ -22,6 +22,6 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
 .onAttach <- function(...) {
-    protoFileLocation <- system.file("inst", "highdim.proto", package = "rtransmartbaseui")
+    protoFileLocation <- system.file("inst", "highdim.proto", package = "rtransmartbaseu")
     readProtoFiles(protoFileLocation)
 }
