38713451b8d287439f2cdd7fadfa04eeb8047c29
jgeofil <jeremy@thehyve.nl>
2015-07-27 12:07:16 +0200

    Functoin

--- R/execscript.R
+++ R/execscript.R
@@ -3,3 +3,9 @@ execscript <- function(text){
   writeLines(text, con="input.R")
   source('input.R', local = TRUE)
 }
+
+execscript2 <- function(text){
+
+  writeLines(text, con="input.R")
+  source('input.R', local = TRUE)
+}
